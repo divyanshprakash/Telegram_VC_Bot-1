e4113170f2d3757a543d6ab98274518b4a565bcf
divyanshprakash <74813755+divyanshprakash@users.noreply.github.com>
2021-08-03 22:37:11 +0530

    Update

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ SESSION_NAME = getenv("SESSION_NAME", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 BOT_USERNAME = getenv("BOT_USERNAME", "")
 admins = {}
-API_ID = int(getenv(""))
+API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH", "")
 
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/c5f676adcdbe5a0bbb232.jpg")
